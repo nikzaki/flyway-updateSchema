e48df27e17062e109dca8356e3dd6a3ee08165b5
Nik Zakiuddin <68600327+nikzaki@users.noreply.github.com>
2025-08-13 20:45:20 +0800

    Update R__gv_league_season_participant.sql

--- wagc_phase1/R__gv_league_season_participant.sql
+++ wagc_phase1/R__gv_league_season_participant.sql
@@ -14,8 +14,12 @@ create or replace view gv_league_season_participant as
            ai.created_by,
            auth_create.name created_by_name,
            ai.last_updated_by,
-           auth_update.name updated_by_name
+           auth_update.name updated_by_name,
+           lstp.fk_league_season_team fk_team
     FROM gs_league_roster lr
+             LEFT JOIN gs_league_season_team_player lstp
+                       ON lstp.fk_league_roster = lr.id
+
              LEFT JOIN gs_competition_player_addl_info ai
                        ON ai.fk_league_season = lr.fk_league_season AND ai.fk_player = lr.fk_player
              LEFT JOIN gs_authentication auth_create
@@ -36,7 +40,8 @@ create or replace view gv_league_season_participant as
            ai.created_by,
            auth_create.name created_by_name,
            ai.last_updated_by,
-           auth_update.name updated_by_name
+           auth_update.name updated_by_name,
+           null as fk_team
     FROM gs_league_season_guest sg
              LEFT JOIN gs_competition_player_addl_info ai
                        ON ai.fk_league_season = sg.fk_league_season AND ai.fk_player = sg.fk_player
